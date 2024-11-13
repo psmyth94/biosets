876a9f2f3183dc3b335ab3923e10c8f091c6f9ab
Patrick Smyth <psmyth1994@gmail.com>
2024-11-13 15:26:05 -0600

    refactor(biodata): simplify target feature creation
    Refactored the schema handling logic to use the _create_target_feature
    method for creating target features. This change simplifies the code and
    ensures consistency in the creation of target features based on the
    provided schema and configuration.

--- src/biosets/packaged_modules/biodata/biodata.py
+++ src/biosets/packaged_modules/biodata/biodata.py
@@ -1348,32 +1348,9 @@ class BioData(datasets.ArrowBasedBuilder):
                                 v.id = self.config.target_column
                                 new_schema[self.TARGET_COLUMN] = v
 
-                    elif is_regression_type(v.pa_type):
-                        new_schema[self.TARGET_COLUMN] = RegressionTarget(
-                            v.dtype, id=self.config.target_column
-                        )
-                    elif self.config.labels and is_classification_type(v.pa_type):
-                        if self.config.positive_labels or self.config.negative_labels:
-                            new_schema[self.TARGET_COLUMN] = BinClassLabel(
-                                positive_labels=self.config.positive_labels,
-                                negative_labels=self.config.negative_labels,
-                                names=self.config.labels,
-                                id=self.config.target_column,
-                            )
-                        else:
-                            new_schema[self.TARGET_COLUMN] = ClassLabel(
-                                num_classes=len(self.config.labels),
-                                names=self.config.labels,
-                                id=self.config.target_column,
-                            )
                     else:
-                        raise ValueError(
-                            "The dataset seems to be a classification task, but the labels are not provided.\n"
-                            "Please provide the labels in the `labels` argument. For example:\n"
-                            "   >>> dataset = dataset.load_dataset('csv', data_files='data.csv', labels=[0, 1, 2])\n"
-                            "Or provide a sample metadata table with the labels column. For example:\n"
-                            "   >>> # metadata.csv contains a column named 'disease state' with labels 0, 1, 2\n"
-                            "   >>> dataset.load_dataset('csv', data_files='data.csv', sample_metadata_files='metadata.csv', target_column='disease state')\n"
+                        new_schema = self._create_target_feature(
+                            new_schema, v.pa_type, return_error=True
                         )
                 elif k == self.METADATA_COLUMN and isinstance(v, dict):
                     new_schema[self.METADATA_COLUMN] = Metadata(feature=v)
